48a5a53e1665fdbff024b803f90c9edde3e556f1
cesarcacho <cesarcacho@gmail.com>
2022-03-29 09:10:06 +0200

    Inclusion de listas

--- PPTLS.py
+++ PPTLS.py
@@ -4,21 +4,23 @@ import random
 cont_maq = 0
 cont_jug = 0
 juego = int(input("cuantas partidas quieres jugar ?"))
-
+lista=["piedra","papel","tijera","lagarto","spock"]
+winer = juego / 2
 while juego != 0:
     jugador = input("Elige tu jugada: ")
-    
-    maquina = random.randint(1, 5)
-    if maquina == 1:
-        contrario = "piedra"
-    elif maquina == 2:
-        contrario = "papel"
-    elif maquina == 3:
-        contrario = "tijera"
-    elif maquina == 4:
-        contrario = "lagarto"
-    elif maquina == 5:
-        contrario = "spock"
+    contrario=lista[random.randint(0,4)]
+# Versión original, luego lo mejoramos con la lista[]
+#    maquina = random.randint(1, 5)
+#    if maquina == 1:
+#        contrario = "piedra"
+#    elif maquina == 2:
+#        contrario = "papel"
+#    elif maquina == 3:
+#        contrario = "tijera"
+#    elif maquina == 4:
+#        contrario = "lagarto"
+#    elif maquina == 5:
+#        contrario = "spock"
     
     if contrario == jugador:
         print("Empate!!! Los dos habéis sacado:", jugador)
@@ -63,6 +65,13 @@ while juego != 0:
     
     print(contrario)
     juego -= 1
+
+    if cont_jug > winer:
+        juego = 0
+        print("Partida GANADA")
+    elif cont_maq > winer:
+        juego = 0
+        print("Partida PERDIDA")
 
 
 print("El resultado final es:")
